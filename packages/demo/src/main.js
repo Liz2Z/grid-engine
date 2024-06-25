7e292916a468714242b83cfd0238eb55d03d9025
Liz2Z <fitz-i@foxmail.com>
2024-06-25 21:02:51 +0800

    fix: zindex

--- packages/demo/src/main.js
+++ packages/demo/src/main.js
@@ -1,8 +1,11 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { GridEngine } from '@lazymonkey/grid-engine-rc';
 
 import App from './App';
 
+GridEngine.settings.NUMBER_OF_COLUMNS = 12;
+GridEngine.settings.ELEMENT_SPACING = 16;
 
 const root = createRoot(document.getElementById('root'));
 
