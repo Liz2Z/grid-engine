6991769d5e624033eaae02fc4243fc47cbe403d6
caolin <caolin_cl@youzan.com>
2024-07-18 19:40:10 +0800

    feat(engine,react-components): 增加网格固定高度方案

--- packages/engine/src/settings.ts
+++ packages/engine/src/settings.ts
@@ -1,14 +1,26 @@
-export const settings = {
+interface ISettings {
   /** 列数量 36列 */
-  NUMBER_OF_COLUMNS: 36,
+  NUMBER_OF_COLUMNS: number;
   /** 行数量 xx 列 */
-  NUMBER_OF_ROWS: -1,
-  
+  NUMBER_OF_ROWS: number;
   /** 元素尺寸 */
+  ELEMENT_SIZE: {
+    width: number;
+    height: number;
+  };
+  /** 元素与元素之间的间隔。单位：px */
+  ELEMENT_SPACING: number;
+  /** 网格高度。单位：px */
+  CELL_HEIGHT?: number;
+}
+
+export const settings: ISettings = {
+  NUMBER_OF_COLUMNS: 36,
+  NUMBER_OF_ROWS: -1,
+
   ELEMENT_SIZE: {
     height: 8,
     width: 8,
   },
-  /** 元素与元素之间的间隔。单位：px */
   ELEMENT_SPACING: 12,
 };
